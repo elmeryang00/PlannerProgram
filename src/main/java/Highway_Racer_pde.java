0de30851b11601179dd7cf7351018f40973d14a7
Tatkinson7 <broncos5405@gmail.com>
2017-04-06 08:58:57 -0500

    Updated Code for Highway Racer 4/6/2017
    Added a dependecy error to highway racer to allow the code to run. Error
    was located in pom.xml. Updated code to add second car to the game.

--- src/main/java/Highway_Racer_pde.java
+++ src/main/java/Highway_Racer_pde.java
@@ -11,25 +11,33 @@ import ddf.minim.ugens.*; */
 //import Highway_Racer_pde.carPosition;
 //import Highway_Racer_pde.roadLine;
 //import Highway_Racer_pde.tree;
- 
+ /*
+  * Dependecy Error for pom.xml
+  * <dependency>
+			<groupId>org.clojars.automata</groupId>
+			<artifactId>jsminim</artifactId>
+			<version>2.1.0</version>
+			
+		</dependency> 
+  */
 
 public class Highway_Racer_pde extends PApplet {
 
 //AudioPlayer[] player = new AudioPlayer[4]; 
 //Minim minim; 
 //FFT fft; 
-//float val; 
-//int val2; 
+float val; 
+int val2; 
 //int bufferSize = 8; 
-//int fftSize = floor(bufferSize*.9f)+1; 
-//int count=0;
+//int fftSize = floor(bufferSize*.9)+1; 
+int count=0;
 PImage lambo,bugatti,wowzer,ow,greenCar;
 PFont dab,daber;
 roadLine a,b,c,d,e,f,g,h,i,j;
 tree r,t;
 carPosition[] garbage=new carPosition[3];
-int[] position =new int[3];
-car wow,ouch;
+int[] position =new int[4];
+car wow,ouch,green;
 int tick,y,gameState,carType;
 int place=1;
 boolean boyz, woz;
@@ -48,18 +56,17 @@ public void start(boolean dab)//input to build an applet
 @Override
 public void setup()
 {
-  woz=false;//sets input to false to stop opening windows, prevents crashing
   gameState=0;
   tick=0;
   y=-144; 
-  /*minim = new Minim(this); 
-  count=0;
-  player[0] = minim.loadFile("Carefree.mp3"); 
-  player[1] = minim.loadFile("jazz.mp3"); 
-  player[2] = minim.loadFile("ericDubstep.wav"); 
-  player[3] = minim.loadFile("extremeaction.mp3"); 
-  player[count].play(); 
-  fft=new FFT(player[count].bufferSize(), player[count].sampleRate()); */
+  //minim = new Minim(this); 
+  count=(int)random(0,4);
+  //player[0] = minim.loadFile("Carefree.mp3"); 
+  //player[1] = minim.loadFile("jazz.mp3"); 
+  //player[2] = minim.loadFile("ericDubstep.wav"); 
+  //player[3] = minim.loadFile("extremeaction.mp3"); 
+  //player[count].play(); 
+  //fft=new FFT(player[count].bufferSize(), player[count].sampleRate()); 
   dab=createFont("Comic Sans MS",32);
   daber=createFont("Arial",12);
   background(0,100,0);
@@ -80,8 +87,10 @@ public void setup()
   position[0]=140;
   position[1]=275;
   position[2]=415;
+  position[3]=0;
   wow=new car(garbage[1],bugatti,"main");
   ouch=new car(position[(int)random(0,3)],-147,ow,"fake");
+  green=new car(position[3],-147,greenCar,"fake");
   r= new tree(0,-144,wowzer);
   t= new tree(501,-144,wowzer);  
   a=new roadLine(225,10,10,100);
@@ -151,37 +160,6 @@ class carPosition
     return ypos;
   }
 }
-class tree
-{
-  int xpos,ypos;
-  PImage i;
-  tree(int x,int y,PImage img)
-  {
-    xpos=x;
-    ypos=y;
-    i=img;
-  }
-  public int getX()
-  {
-    return xpos;
-  }
-  public int getY()
-  {
-    return ypos;
-  }
-  public void draw()
-  {
-    image(i,xpos,ypos);
-  }
-  public void move()
-  {
-    ypos+=13;
-    if(ypos>800)
-    {
-      ypos=-144;
-    }
-  }
-}
 class car
 {
   int xpos,ypos,speed,go;
@@ -226,24 +204,43 @@ class car
   {
       image(i, xpos, ypos);
   }
-  public void move()
+  public void changePos(int x)
   {
+    int other=x;
+    int a=(int)random(1,4);
+      //println(a);
+      if(a==1&&(other!=140))
+      {
+        xpos=140;//position[0];
+      }
+      else if(a==2&&(other!=275))
+      {
+        xpos=275;//position[1];
+      }
+      else if(a==3&&(other!=415))
+      {
+        xpos=415;//position[2];
+      }
+  }
+  public void move(int x)
+  {
+    int other=x;
     ypos+=speed;
     if(ypos>800)
-    {
+    {     
       int a=(int)random(1,4);
-      println(a);
-      if(a==1)
+      //println(a);
+      if(a==1&&(other!=140))
       {
-        xpos=position[0];
+        xpos=140;//position[0];
       }
-      if(a==2)
+      else if(a==2&&(other!=275))
       {
-        xpos=position[1];
+        xpos=275;//position[1];
       }
-      if(a==3)
+      else if(a==3&&(other!=415))
       {
-        xpos=position[2];
+        xpos=415;//position[2];
       }
       ypos=-144;
       if(speed<20)
@@ -254,90 +251,167 @@ class car
     }
   }
 }
-@Override
+class tree
+{
+  int xpos,ypos,speed;
+  PImage i;
+  tree(int x,int y,PImage img)
+  {
+    xpos=x;
+    ypos=y;
+    i=img;
+    speed=5;
+  }
+  public int getX()
+  {
+    return xpos;
+  }
+  public int getY()
+  {
+    return ypos;
+  }
+  public void draw()
+  {
+    image(i,xpos,ypos);
+  }
+  public void move()
+  {
+    ypos+=speed;
+    if(ypos>800)
+    {
+      ypos=-144;
+    }
+    if(speed<20)
+    {
+      speed++;
+    }
+  }
+}
+
 public void draw()
 {
-	/*if(!player[count].isPlaying()) 
-	  { 
-	    count++; 
-	    if(count>3)count=0; 
-	    player[count].play();    
-	  }     */
-	  if(gameState==0)
-	  {
-	    image(bugatti,100,300);
-	    image(lambo,450,300);
-	  }
-	  if(gameState==1)
-	  {
-	    if(y>850)
-	    {
-	      y=-144;
-	    }
-	    
-	    tick++;
-	    drawMap();
-	    wow.draw();
-	    ouch.draw();
-	    ouch.move();
-	    r.draw();
-	    t.draw();
-	    r.move();
-	    t.move();
-	    a.drawLine();
-	    b.drawLine();
-	    c.drawLine();
-	    d.drawLine();
-	    e.drawLine();
-	    f.drawLine();
-	    g.drawLine();
-	    h.drawLine();
-	    i.drawLine();
-	    j.drawLine();
-	    fill(255);
-	    textAlign(CENTER);
-	    textFont(daber);
-	    //text(tick, 300, 50);
-	    text(tick/60,300,50);
-	    if(tick%15==0&&place==1)
-	    {
-	      a.moveDown();
-	      b.moveDown();
-	      c.moveDown();
-	      d.moveDown();
-	      e.moveDown();
-	      f.moveDown();
-	      g.moveDown();
-	      h.moveDown();
-	      i.moveDown();
-	      j.moveDown();
-	      place=2;
-	    }
-	    else if(tick%15==0&&place==2)
-	    {
-	      a.moveUp();
-	      b.moveUp();
-	      c.moveUp();
-	      d.moveUp();
-	      e.moveUp();
-	      f.moveUp();
-	      g.moveUp();
-	      h.moveUp();
-	      i.moveUp();
-	      j.moveUp();
-	      place=1;
-	    }
-	    if(((wow.getY()>=ouch.getY()&&wow.getY()<=ouch.getY()+97)||(wow.getY()+110>=ouch.getY()&&wow.getY()+110<=ouch.getY()+97)||(wow.getY()+55>=ouch.getY()&&wow.getY()+55<=ouch.getY()+97))&&wow.getX()==ouch.getX())
-	    {
-	      fill(50);
-	      rect(250,25,100,50);
-	      fill(255);
-	      text("Click to Start Over",300,50);
-	      text("YOU LOSE",300,100);
-	      text("Time: " + tick/60,300,150);
-	      gameState=2;
-	    }
-	  }
+  /*if(!player[count].isPlaying()) 
+  { 
+    count++; 
+    if(count>3)count=0; 
+    player[count].play();    
+  }  */   
+  if(gameState==0)
+  {
+    image(bugatti,100,300);
+    image(lambo,450,300);
+  }
+  if(gameState==1)
+  {
+    if(y>850)
+    {
+      y=-144;
+    }
+    if(tick/60==10)
+    {
+      green=new car(position[(int)random(0,3)],-147,greenCar,"fake");
+    }
+    tick++;
+    drawMap();
+    wow.draw();
+    //ouch.draw();
+    if((green.getX()!=ouch.getX())&&tick/60>10)
+    {
+      green.draw();
+      green.move(ouch.getX());
+      ouch.draw();
+      ouch.move(green.getX());
+      println("lets go"+tick);
+    } 
+    else if((green.getX()==ouch.getX())&&tick/60>10)
+    {
+      green.changePos(ouch.getX());
+      println("nope"+tick);
+      green.draw();
+      green.move(ouch.getX());
+      ouch.draw();
+      ouch.move(green.getX());
+    } 
+    if(tick/60<10)
+    {
+      ouch.draw();
+      ouch.move(green.getX());
+    }
+    println("zoom"+tick);
+    //ouch.move(green.getX());
+    r.draw();
+    if(tick/60>1)
+    {
+      t.draw();
+      t.move();
+    }
+    r.move();
+    a.drawLine();
+    b.drawLine();
+    c.drawLine();
+    d.drawLine();
+    e.drawLine();
+    f.drawLine();
+    g.drawLine();
+    h.drawLine();
+    i.drawLine();
+    j.drawLine();
+    h.drawLine();
+    d.drawLine();
+    fill(255);
+    textAlign(CENTER);
+    textFont(daber);
+    //text(tick, 300, 50);
+    text(tick/60,300,50);
+    if(tick%15==0&&place==1)
+    {
+      a.moveDown();
+      b.moveDown();
+      c.moveDown();
+      d.moveDown();
+      e.moveDown();
+      f.moveDown();
+      g.moveDown();
+      h.moveDown();
+      i.moveDown();
+      j.moveDown();
+      place=2;
+    }
+    else if(tick%15==0&&place==2)
+    {
+      a.moveUp();
+      b.moveUp();
+      c.moveUp();
+      d.moveUp();
+      e.moveUp();
+      f.moveUp();
+      g.moveUp();
+      h.moveUp();
+      i.moveUp();
+      j.moveUp();
+      place=1;
+    }
+    if(((wow.getY()>=ouch.getY()&&wow.getY()<=ouch.getY()+97)||(wow.getY()+110>=ouch.getY()&&wow.getY()+110<=ouch.getY()+97)||(wow.getY()+55>=ouch.getY()&&wow.getY()+55<=ouch.getY()+97))&&wow.getX()==ouch.getX())
+    {
+      fill(50);
+      rect(250,25,100,50);
+      fill(255);
+      text("Click to Start Over",300,50);
+      text("YOU LOSE",300,100);
+      text("Time: " + tick/60,300,150);
+      gameState=2;
+    }
+    else if(((wow.getY()>=green.getY()&&wow.getY()<=green.getY()+97)||(wow.getY()+110>=green.getY()&&wow.getY()+110<=green.getY()+97)||(wow.getY()+55>=green.getY()&&wow.getY()+55<=green.getY()+97))&&wow.getX()==green.getX())
+    {
+      fill(50);
+      rect(250,25,100,50);
+      fill(255);
+      text("Click to Start Over",300,50);
+      text("YOU LOSE",300,100);
+      text("Time: " + tick/60,300,150);
+      gameState=2;
+    }
+  }
 }
 public void drawMap()
 {
@@ -345,77 +419,75 @@ public void drawMap()
   fill(50);
   rect(100,0,400,800);
 }
-@Override
 public void keyPressed() {
-	  if (key == CODED) {
-	    /*if (keyCode == UP&&(gameState==0||gameState==1||gameState==2))
-	    {
-	      player[count].rewind();
-	      player[count].pause();
-	      count++;
-	      if(count>3)count=0; 
-	      player[count].play();
-	    }*/
-	    if (keyCode == RIGHT&&carType==1) 
-	    {
-	      
-	      if(wow.getX()==140)
-	      {
-	        //println("dab on em");
-	        wow= new car(garbage[1],bugatti,"main");
-	      }
-	      else if(wow.getX()==275)
-	      {
-	        //println("dab on em");
-	        wow= new car(garbage[2],bugatti,"main");      
-	      }
-	    } else if (keyCode == LEFT&&carType==1) 
-	    {
-	      
-	      if(wow.getX()==275)
-	      {
-	        //println("dab on em");
-	        wow= new car(garbage[0],bugatti,"main");
-	      }
-	      if(wow.getX()==415)
-	      {
-	        //println("dab on em");
-	        wow= new car(garbage[1],bugatti,"main");        
-	      }
-	    } 
-	    if (keyCode == RIGHT&&carType==2) 
-	    {
-	      
-	      if(wow.getX()==140)
-	      {
-	        //println("dab on em");
-	        wow= new car(garbage[1],lambo,"main");
-	      }
-	      else if(wow.getX()==275)
-	      {
-	        //println("dab on em");
-	        wow= new car(garbage[2],lambo,"main");      
-	      }
-	    } else if (keyCode == LEFT&&carType==2) 
-	    {
-	      
-	      if(wow.getX()==275)
-	      {
-	        //println("dab on em");
-	        wow= new car(garbage[0],lambo,"main");
-	      }
-	      if(wow.getX()==415)
-	      {
-	        //println("dab on em");
-	        wow= new car(garbage[1],lambo,"main");        
-	      }
-	    }
-	  } 
-	  else 
-	  {
-	  }
-	}
-@Override
+  if (key == CODED) {
+    if (keyCode == UP&&(gameState==0||gameState==1||gameState==2))
+    {
+      //player[count].rewind();
+      //player[count].pause();
+      //count++;
+      //if(count>3)count=0; 
+      //player[count].play();
+    }
+    if (keyCode == RIGHT&&carType==1) 
+    {
+      
+      if(wow.getX()==140)
+      {
+        //println("dab on em");
+        wow= new car(garbage[1],bugatti,"main");
+      }
+      else if(wow.getX()==275)
+      {
+        //println("dab on em");
+        wow= new car(garbage[2],bugatti,"main");      
+      }
+    } else if (keyCode == LEFT&&carType==1) 
+    {
+      
+      if(wow.getX()==275)
+      {
+        //println("dab on em");
+        wow= new car(garbage[0],bugatti,"main");
+      }
+      if(wow.getX()==415)
+      {
+        //println("dab on em");
+        wow= new car(garbage[1],bugatti,"main");        
+      }
+    } 
+    if (keyCode == RIGHT&&carType==2) 
+    {
+      
+      if(wow.getX()==140)
+      {
+        //println("dab on em");
+        wow= new car(garbage[1],lambo,"main");
+      }
+      else if(wow.getX()==275)
+      {
+        //println("dab on em");
+        wow= new car(garbage[2],lambo,"main");      
+      }
+    } else if (keyCode == LEFT&&carType==2) 
+    {
+      
+      if(wow.getX()==275)
+      {
+        //println("dab on em");
+        wow= new car(garbage[0],lambo,"main");
+      }
+      if(wow.getX()==415)
+      {
+        //println("dab on em");
+        wow= new car(garbage[1],lambo,"main");        
+      }
+    }
+  } 
+  else 
+  {
+  }
+}
 public void mouseClicked()
 {
   /*if((mouseX>=247&&mouseX<=347)&&(mouseY>=323&&mouseY<=373)&&gameState==0)
@@ -425,7 +497,7 @@ public void mouseClicked()
   }*/
   if((mouseX>=250&&mouseX<=350)&&(mouseY>=25&&mouseY<=75)&&gameState==2)
   {
-   // player[count].pause();
+    //player[count].pause();
     setup();
   }
   if((mouseX>=100&&mouseX<=150)&&(mouseY>=300&&mouseY<=400)&&gameState==0)
